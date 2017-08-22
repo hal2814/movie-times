7872896e122d0036a2bb0b7fe3c7979554ed66f1
Dan Danilyuk and Nate McGregor <nate@>
2017-08-22 13:20:50 -0700

    created javascript objects and prototypes

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,44 @@
+function ticket(movie){
+  this.movie = movie;
+  this.ticketInfos = [];
+}
 
+function ticketInfo(age,time){
+  this.age = age;
+  this.time = time;
+}
+
+
+ticketInfo.prototype.fullInfo = function(){
+  return this.age + " " + this.time;
+};
+
+ticket.prototype.fullTicket = function () {
+    return this.movie + " " + ticketInfos.fullInfo();
+};
+
+
+function selectMovie(select){
+  if(select === "dunkirk"){
+    $("#dunkirk").show();
+    $("#logan").hide();
+    $("#emoji").hide();
+  }
+  if(select === "logan"){
+    $("#dunkirk").hide();
+    $("#logan").show();
+    $("#emoji").hide();
+  }
+  if(select === "emoji"){
+    $("#dunkirk").hide();
+    $("#logan").hide();
+    $("#emoji").show();
+  }
+};
+
+$(document).ready(function() {
+  $("#movie-select").change(function(){
+      var movie_selection = $("#movie-select").val();
+      selectMovie(movie_selection);
+    });
+});
