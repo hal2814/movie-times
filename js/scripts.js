563d2aa83f5618be0315320818a8b2c15c0f835a
Dan Danilyuk and Nate McGregor <dan@>
2017-08-22 14:07:12 -0700

    populate objects with data, output data, calculate movie price

--- js/scripts.js
+++ js/scripts.js
@@ -10,35 +10,60 @@ function ticketInfo(age,time){
 
 
 ticketInfo.prototype.fullInfo = function(){
-  return this.age + " " + this.time;
+  return "<li>" + this.age + "</li> " + "<li>" + this.time + "</li>";
 };
 
-ticket.prototype.fullTicket = function () {
-    return this.movie + " " + ticketInfos.fullInfo();
+ticket.prototype.fullTicket = function(info) {
+    return "<li>" + this.movie + "</li>" + info.fullInfo();
 };
+
+function priceCalculator(time,age){
+  var price = 10;
+  if(time !== "11:00 a.m."){
+    price += 2;
+  }
+  if(age === "Adult"){
+    price += 3;
+  }else if(age === "Student"){
+    price += 1;
+  }
+  return price;
+}
 
 
 function selectMovie(select){
-  if(select === "dunkirk"){
+  if(select === "Dunkirk"){
     $("#dunkirk").show();
     $("#logan").hide();
     $("#emoji").hide();
   }
-  if(select === "logan"){
+  if(select === "Logan Lucky"){
     $("#dunkirk").hide();
     $("#logan").show();
     $("#emoji").hide();
   }
-  if(select === "emoji"){
+  if(select === "The Emoji Movie"){
     $("#dunkirk").hide();
     $("#logan").hide();
     $("#emoji").show();
   }
+  $("#time-age-show").show();
 };
 
 $(document).ready(function() {
+  var newTicket;
   $("#movie-select").change(function(){
-      var movie_selection = $("#movie-select").val();
-      selectMovie(movie_selection);
-    });
+    var movie_selection = $("#movie-select").val();
+    selectMovie(movie_selection);
+    newTicket = new ticket(movie_selection);
+  });
+  $("form#movieForm").submit(function(event) {
+    event.preventDefault();
+    var age = $("#movie-age").val();
+    var time = $("#movie-time").val();
+    var newTicketInfo = new ticketInfo(age,time);
+    newTicket.ticketInfos.push(newTicketInfo);
+    $("#output").append(newTicket.fullTicket(newTicketInfo));
+    $("#output").append("<li> Your total is: $" + priceCalculator(time,age) + "</li>");
+  });
 });
